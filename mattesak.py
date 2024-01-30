2b81d9a148658470c9acb7a7e2f0e14dd475a4fa
liam <128035960+olopenjo@users.noreply.github.com>
2024-01-30 11:01:01 +0100

    Add files via upload

---
+++ mattesak.py
@@ -0,0 +1,20 @@
+while True:
+    print("upphöjt matte!!!")
+
+    a = int(input("Skriv första talet "))
+
+    b = int(input("Skriv andra talet \n"))
+
+    c = int(input("Skriv tredje talet \n"))
+
+    summa = (a*a)+(b*b) 
+
+    if summa == c*c:
+
+        print("bra det är rätt!!")
+
+        print("talet är", c*c)
+
+    else:
+
+        print("dåligt stämmer inte")
